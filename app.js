b8f86279e57a1f6717e8d53f038e4e5c178652e8
Camzijss <58549418+Camzijss@users.noreply.github.com>
2020-05-14 16:36:56 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
 $(document).ready(function() { 
 
-    $("#main_page").css("display", "block");
-    $("#work_page").css("display", "none");
-    $("#about_page").css("display", "none");
-    $("#contacts_page").css("display", "none");
+        $("#main_page").css("display", "block");
+        $("#work_page").css("display", "none");
+        $("#about_page").css("display", "none");
+        $("#contacts_page").css("display", "none");
 
-    $(".navigācijas-poga").click(function() {
-        var text = $(this).text();
+        $(".work").click(function() {
+            $("#main_page, #about_page, #contacts_page").hide();
+            $("#work_page").show();
+            $("#main-body").css("background-image", "url(resources/fons_darbi.jpg)");
+        });
 
-        if (text === "PAR MANI"){
-            $("#main_page").css("display", "none");
-            $("#work_page").css("display", "none");
-            $("#about_page").css("display", "block");
-            $("#contacts_page").css("display", "none");
-        }
+        $(".mani").click(function() {
+            $("#main_page, #work_page, #contacts_page").hide();
+            $("#about_page").show();
+            $("#main-body").css("background-image", "url(resources/fons_mani.jpg)");
+        });
 
-        if (text === "MAIKLS MĀRIS TRĒGERS"){
-            $("#main_page").css("display", "block");
-            $("#work_page").css("display", "none");
-            $("#about_page").css("display", "none");
-            $("#contacts_page").css("display", "none");
-        }
+        $(".home").click(function() {
+            $("#about_page, #work_page, #contacts_page").hide();
+            $("#main_page").show();
+            $("#main-body").css("background-image", "url('resources/fons_start.png')");
+        });
 
-        if (text === "MANI DARBI"){
-            $("#main_page").css("display", "none");
-            $("#work_page").css("display", "block");
-            $("#about_page").css("display", "none");
-            $("#contacts_page").css("display", "none");
-        }
-
-        if (text === "KONTAKTI"){
-            $("#main_page").css("display", "none");
-            $("#work_page").css("display", "none");
-            $("#about_page").css("display", "none");
-            $("#contacts_page").css("display", "block");
-        }
-
-
-    });
-
+        $(".kont").click(function() {
+            $("#main_page, #work_page, #about_page").hide();
+            $("#contacts_page").show();
+            $("#main-body").css("background-image", "url(resources/fons_kont.jpg)");
+        });
 });
